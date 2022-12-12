c6a20002b5f7670cfd1375a2f29143837f74f3b6
emsesc <emseschen@gmail.com>
2022-12-12 14:57:34 -0500

    simplify lambda

--- rgb2hex/index.mjs
+++ rgb2hex/index.mjs
@@ -1,46 +1,25 @@
-import fetch from 'node-fetch';
-import rgbHex from 'rgb-hex';
-
-const SECRET = ""
+// Set up API Gateway
 
 export const handler = async(event) => {
     // TODO implement
     
-    const location = event["queryStringParameters"]["location"];
-    const username = event["queryStringParameters"]["username"];
-    
-    const res = await fetch(`https://meet-mako-28.hasura.app/api/rest/location/${location}`, { 
-        headers: {
-          'x-hasura-admin-secret': SECRET
-        } 
-    });
-    const data = await res.json();
-    
-    const rgb = data.art_pixels[0].rgb_value.split(",")
+    const rgb = event["queryStringParameters"]["rgb"];
     console.log(rgb)
     
-    const hex = rgbHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2]));
-    
-    const body = {
-        "username": username,
-        "loc": location,
-        "hex_value": hex
-    };
-
-    const res2 = await fetch('https://meet-mako-28.hasura.app/api/rest/steal', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'x-hasura-admin-secret': SECRET
-        },
-        body: JSON.stringify(body)
-    });
-
-    const json = await res2.json();
+    const hex = rgbToHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2]));
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(json),
+        body: JSON.stringify({"hex": hex}),
     };
     return response;
 };
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
