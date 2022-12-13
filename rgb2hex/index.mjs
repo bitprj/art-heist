5b72cc2be4a699dc087a35029696e3c652d526e4
emsesc <emseschen@gmail.com>
2022-12-12 21:44:33 -0500

    fix lambda

--- rgb2hex/index.mjs
+++ rgb2hex/index.mjs
@@ -1,25 +1,23 @@
-// Set up API Gateway
+export const handler = async (event) => {
+  // TODO implement
 
-export const handler = async(event) => {
-    // TODO implement
-    
-    const rgb = event["queryStringParameters"]["rgb"];
-    console.log(rgb)
-    
-    const hex = rgbToHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2]));
+  const rgb = event["queryStringParameters"]["rgb"].split(",");
+  console.log(rgb)
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({"hex": hex}),
-    };
-    return response;
+  const hex = rgbToHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2]));
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ "hex": hex }),
+  };
+  return response;
 };
 
-function componentToHex(c) {
+function valueToHex(c) {
   var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
+  return hex
 }
 
 function rgbToHex(r, g, b) {
-  return componentToHex(r) + componentToHex(g) + componentToHex(b);
+  return (valueToHex(r) + valueToHex(g) + valueToHex(b));
 }
