1022ea47e03ceedc8af1081c3ea282d2f7ae617a
emsesc <emseschen@gmail.com>
2022-12-14 22:23:25 -0500

    api/check endpoint works

--- frontend/pages/api/check.js
+++ frontend/pages/api/check.js
@@ -2,23 +2,47 @@ import fetch from 'node-fetch';
 import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 
-for (let i = 1; i < 50; i++) {
-    let { data: art_pixels, error } = await supabase
-    .from('art_pixels')
-    .select("rgb_value")
-    .eq('location', i)
+export default async function handler(req, res) {
+    console.log(req.body)
+    const { start, end, url, username } = req.body;
+    let response = ""
+    let incorrectValue = false;
 
-    console.log(art_pixels)
-    const rgb = art_pixels[0].rgb_value
+    try {
+        for (let i = start; i <= end; i++) {
+            let { data: art_pixels, error } = await supabase
+                .from('art_pixels')
+                .select('rgb_value, correct_hex')
+                .eq('location', i)
+    
+            console.log(art_pixels)
+            const rgb = art_pixels[0].rgb_value
+    
+            const res = await fetch(`${url}?rgb=${rgb}`);
+            const hex = await res.json()
+            console.log(hex.hex)
 
-    const res = await fetch(`https://o48pvkru03.execute-api.us-east-1.amazonaws.com/rgb2hex?rgb=${rgb}`);
-    const hex = await res.json()
-    console.log(hex.hex)
+            if (art_pixels[0].correct_hex != hex.hex) {
+                incorrectValue = true;
+            }
+    
+            const { data, err } = await supabase
+                .from('art_pixels')
+                .update({ username: username, hex_value: hex.hex })
+                .eq('location', i)
+    
+            console.log(data);
+        }
+        
+        if (incorrectValue) {
+            response = `Successfully requested Lambda function, but the hex values are incorrect.`;
+        } else {
+            response = `Successfully requested Lambda function, and the hex values are correct!`;
+        }
 
-    const { data, err } = await supabase
-        .from('art_pixels')
-        .update({ username: 'test', hex_value: hex.hex })
-        .eq('location', i)
+    } catch (e) {
+        response = `There was an issue requesting your Lambda function. Here was the error: ${e}`;
+    }
 
-    console.log(data)
+    res.status(200).json({ message: response })
 }
