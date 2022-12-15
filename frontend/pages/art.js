ad699b8f9118a100e38bc66ca9d531cd6753ffa2
emsesc <emseschen@gmail.com>
2022-12-15 10:13:03 -0500

    quesiton mark icon for empty pixels

--- frontend/pages/art.js
+++ frontend/pages/art.js
@@ -2,8 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { Popover, PopoverTrigger, PopoverContent, Box } from '@chakra-ui/core';
 import { Center, SimpleGrid, Text } from '@chakra-ui/react';
 import { createClient } from '@supabase/supabase-js';
+import { QuestionIcon } from '@chakra-ui/icons';
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
+function Pixel(hexValue) {
+  if (hexValue.hex_value == null) {
+    return (
+      <QuestionIcon />
+    );
+  } else {
+    return (
+      <Popover trigger="hover" matchWidth="true">
+        <PopoverTrigger>
+          <Box _hover={{ bg: "#fff" }} p={2} bg={"#" + hexValue.hex_value}>
+          </Box>
+        </PopoverTrigger>
+        <PopoverContent boxShadow='dark-lg' borderRadius="6px" pl="3" pr="3" borderWidth="1px" borderColor="gray.500" opacity="0.8" w="auto">
+          <Text color="black"><b>User: </b>{hexValue.username}<br></br><b>Time: </b>{hexValue.updated_at} EST</Text>
+        </PopoverContent>
+      </Popover>
+    );
+  }
+
+}
 
 const Art = () => {
   const [hexValues, setHexValues] = useState([])
@@ -41,17 +63,7 @@ const Art = () => {
   return (
     <Center bg='black' w='calc(100vw)' h='calc(100vh)' color='white'>
       <SimpleGrid spacingX='0px' spacingY='0px' w='auto' h='auto' columns={32}>
-        {hexValues.map(hexValue => (
-          <Popover trigger="hover" matchWidth="true">
-            <PopoverTrigger>
-              <Box _hover={{bg: "#fff"}} p={2} bg={"#" + hexValue.hex_value}>
-              </Box>
-            </PopoverTrigger>
-            <PopoverContent boxShadow='dark-lg' borderRadius="6px" pl="3" pr="3" borderWidth="1px" borderColor="gray.500" opacity="0.8" w="auto">
-                  <Text color="black"><b>User: </b>{hexValue.username}<br></br><b>Time: </b>{hexValue.updated_at} EST</Text>
-            </PopoverContent>
-          </Popover>
-        ))}
+        {hexValues.map(hexValue => Pixel(hexValue))}
       </SimpleGrid>
     </Center>
   );
