3d2199a023d227f2fb4f9976fa3c8b06fd0d2ebd
Daniel Kim <dgkim@ucdavis.edu>
2022-12-16 12:53:54 +0000

    you can't visit /test without logging in first

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,39 @@
 import '../styles/globals.css'
-import { ClerkProvider } from '@clerk/nextjs';
+import {ClerkLoaded, ClerkProvider, SignedIn, SignedOut} from "@clerk/nextjs";
+import { ChakraProvider } from '@chakra-ui/react'
+import Header from "../components/Header.jsx";
+import {useRouter} from "next/router";
+
+const publicPages = ["/", "/art"];
+
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
-    <ClerkProvider {...pageProps} >
-      <Component {...pageProps} />
-   </ClerkProvider>
+    <ChakraProvider>
+    <ClerkProvider {...pageProps}>
+      <ClerkLoaded>
+        <Header />
+        {publicPages.includes(router.pathname) ? (
+          <main>
+            <Component {...pageProps} />
+          </main>
+        ) : (
+          <>
+            <SignedIn>
+              <Component {...pageProps} />
+            </SignedIn>
+            <SignedOut>
+              <div className="protected">
+                <p>You need to be signed in to access this page.</p>
+              </div>
+            </SignedOut>
+          </>
+        )}
+       </ClerkLoaded>
+      </ClerkProvider>
+   </ChakraProvider>
   );
 }
 
