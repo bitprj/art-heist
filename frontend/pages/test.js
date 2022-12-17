8c3081dcecbbf965b30aef3102c04d13aca93b16
emsesc <emseschen@gmail.com>
2022-12-16 21:26:29 -0500

    made page look pretty

--- frontend/pages/test.js
+++ frontend/pages/test.js
@@ -1,4 +1,4 @@
-import { VStack, Flex, Button, Input, Text, Spinner, Center, TableContainer, Thead, Tr, Th, TableCaption, Td, Table, Tbody } from '@chakra-ui/react';
+import { Box, VStack, Flex, Button, Input, Text, Spinner, Center, TableContainer, Thead, Tr, Th, TableCaption, Td, Table, Tbody } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import fetch from 'node-fetch';
 import { useUser } from '@clerk/nextjs'
@@ -8,40 +8,42 @@ function Loading(loading, message, cases) {
   if (message) {
     console.log(cases);
     return (
-      <VStack spacing={4}>
-        <Text mt={4}>{message}</Text>
-        <Center>
-          <TableContainer>
-            <Table variant='simple'>
-              <TableCaption placement="top">Test Cases</TableCaption>
-              <Thead>
-                <Tr>
-                  <Th>Status</Th>
-                  <Th>Expected hex value</Th>
-                  <Th>Received hex value</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {cases.map(c => (
+      <Box pl="30px" pr="30px" w="auto" borderWidth='1px' borderRadius='lg' overflow='hidden' boxShadow="xl">
+        <VStack spacing={4}>
+          <Text mt={4}>{message}</Text>
+          <Center>
+            <TableContainer>
+              <Table variant='simple'>
+                <TableCaption placement="top">Test Cases</TableCaption>
+                <Thead>
                   <Tr>
-                    <Td>{c.status}</Td>
-                    <Td>#{c.correct_hex}</Td>
-                    <Td>#{c.received_hex}</Td>
+                    <Th>Status</Th>
+                    <Th>Expected hex value</Th>
+                    <Th>Received hex value</Th>
                   </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Center>
-      </VStack>
+                </Thead>
+                <Tbody>
+                  {cases.map(c => (
+                    <Tr>
+                      <Td>{c.status}</Td>
+                      <Td>#{c.correct_hex}</Td>
+                      <Td>#{c.received_hex}</Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Center>
+        </VStack>
+      </Box>
     );
   } else if (loading) {
     return (
       <Spinner
         thickness='15px'
         speed='0.65s'
-        emptyColor='#E6FFFA'
-        color='#285E61'
+        emptyColor='#ccd6fc'
+        color='#325BF1'
         size='xl'
       />
     );
@@ -83,17 +85,26 @@ const Test = () => {
 
   return (
     <Flex pl="280px" pr="280px" direction="column" alignItems="center" mt={8}>
-      <Text htmlFor="input" fontSize="xl">Welcome, <b>{user.firstName}</b>! Enter your Lambda Function's endpoint:</Text>
+      <Flex>
+        <Text htmlFor="input" fontSize="5xl">Welcome,&nbsp;</Text>
+        <Text htmlFor="input" fontSize="5xl" color="#325BF1"><b>{user.firstName}</b></Text>
+        <Text htmlFor="input" fontSize="5xl">!</Text>
+      </Flex>
+      <Text htmlFor="input" fontSize="md">(aka <b>@{user.username}</b>)</Text>
+      <br></br>
+      <Text htmlFor="input" fontSize="xl">Enter your Lambda function's endpoint:</Text>
       <Input
         id="input"
         type="text"
         value={inputValue}
         onChange={(event) => setInputValue(event.target.value)}
+        placeholder="Your Lambda function's URL"
       />
       <br></br>
       <Button type="submit" onClick={handleSubmit}>
         Submit
       </Button>
+      <br></br>
       {Loading(loading, message, cases)}
     </Flex>
   );
